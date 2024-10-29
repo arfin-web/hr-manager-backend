fb2cc0928d43173a9864ecf63a00d9d3a06c1af1
arfin-web <arfinpriom12@gmail.com>
2024-10-29 18:21:07 +0600

    middleware added in auth feature

--- src/features/auth/auth.controller.js
+++ src/features/auth/auth.controller.js
@@ -37,7 +37,23 @@ const login = async (req, res) => {
     }
 }
 
+const getProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password")
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
+        }
+        res.status(200).json({
+            message: "User profile fetched successfully",
+            data: user
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching profile data" })
+    }
+}
+
 module.exports = {
     register,
-    login
+    login,
+    getProfile
 }
--- src/features/auth/auth.route.js
+++ src/features/auth/auth.route.js
@@ -1,9 +1,11 @@
 const express = require("express")
-const { register, login } = require("./auth.controller")
+const { register, login, getProfile } = require("./auth.controller")
+const authMiddleware = require("./auth.middleware")
 
 const router = express.Router()
 
 router.post("/register", register)
 router.post("/login", login)
+router.get("/profile", authMiddleware, getProfile)
 
 module.exports = router
