0331435ea83beac2cc75014f3b4a00460b03ee47
arfin-web <arfinpriom12@gmail.com>
2024-10-29 15:55:57 +0600

    Auth Feature Created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
+const dbConnect = require("./config/dbConnect")
+const authRoutes = require("./features/auth/auth.route")
+
+dbConnect()
 
 const app = express()
 
 // Middleware
 app.use(express.json())
+
+// Routes
+app.use("/api/v1/auth", authRoutes)
 
 // Start the Server
 const PORT = process.env.PORT || 5000
