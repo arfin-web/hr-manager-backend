d981e339e81195c21a1412076cb7797763816915
arfin-web <arfinpriom12@gmail.com>
2024-10-31 09:20:34 +0600

    cors added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
+const cors = require("cors");
 const dbConnect = require("./config/dbConnect")
 const authRoutes = require("./features/auth/auth.route")
 const employeesRoute = require("./features/employees/employees.route")
@@ -12,6 +13,7 @@ const app = express()
 
 // Middleware
 app.use(express.json())
+app.use(cors());
 
 // Routes
 app.use("/api/v1/auth", authRoutes)
@@ -19,8 +21,13 @@ app.use("/api/v1", employeesRoute)
 app.use("/api/v1", noticeRoute)
 app.use("/api/v1", tasksRoute)
 
+// Define Root Get Route
+app.get("/", (req, res) => {
+    res.send("Welcome to the API")
+})
+
 // Start the Server
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
