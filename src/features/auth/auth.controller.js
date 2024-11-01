97236d2f2866e4d6a35ae8ed790e548e3edb0df0
arfin-web <arfinpriom12@gmail.com>
2024-11-01 12:05:25 +0600

    auth token expire time updated

--- src/features/auth/auth.controller.js
+++ src/features/auth/auth.controller.js
@@ -29,7 +29,7 @@ const login = async (req, res) => {
             id: user._id,
             role: user.role
         }, process.env.JWT_SECRET, {
-            expiresIn: "1h"
+            expiresIn: "3h"
         })
         res.status(200).json({ token })
     } catch (error) {
